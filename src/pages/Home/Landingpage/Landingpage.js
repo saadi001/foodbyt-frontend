0608e026f746a1f5b74b19f61c8ed93393751e48
Sheikh sadi <sheikhsadi1620@gmail.com>
2023-03-09 02:53:49 +0600

    landing page updated

--- src/pages/Home/Landingpage/Landingpage.js
+++ src/pages/Home/Landingpage/Landingpage.js
@@ -1,9 +1,18 @@
+import { MapPinIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 
 const Landingpage = () => {
      return (
-          <div>
-               this is landing page
+          <div className='min-h-screen border bg-[#FEF1F1] flex'>
+               <div className='w-1/2 border flex items-center'>
+                    <div>
+                         <p className='text-xl font-medium flex items-center gap-1'><MapPinIcon className='w-5 h-5 text-[#EA2A00]'></MapPinIcon> Daffodil International University</p>
+                         <p className='text-5xl font-semibold my-3'>Order your favourite Iftar <br /> From Home</p>
+                    </div>
+               </div>
+               <div className='w-1/2'>
+
+               </div>
           </div>
      );
 };
--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -1,54 +1,68 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../Asset/logo/logo1.png'
+import { ShoppingBagIcon, ShoppingCartIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
+     const menu = <>
+          <li><a>Home</a></li>
+          <li tabIndex={0}>
+               <a className="justify-between">
+                    categories
+                    <svg className="fill-current rotate-90" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+               </a>
+               <ul className="p-2">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
+               </ul>
+          </li>
+          <li><a>Find shop</a></li>
+          <li><a>Contact us</a></li>
+          <li tabIndex={0}>
+               <a className="justify-between">
+                    account
+                    <svg className="fill-current rotate-90" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
+               </a>
+               <ul className="p-2">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
+               </ul>
+          </li>
+     </>
      return (
-          <div>
-               <div className="navbar gap-3">
-                    <div className="border">
+          <div className='fixed left-0 right-0'>
+               <div className="navbar justify-between gap-3  lg:px-8">
+                    <div className="">
                          <div className="dropdown">
                               <label tabIndex={0} className="btn btn-ghost lg:hidden">
                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                               </label>
                               <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                                   <li><a>Item 1</a></li>
-                                   <li tabIndex={0}>
-                                        <a className="justify-between">
-                                             Parent
-                                             <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                                        </a>
-                                        <ul className="p-2">
-                                             <li><a>Submenu 1</a></li>
-                                             <li><a>Submenu 2</a></li>
-                                        </ul>
-                                   </li>
-                                   <li><a>Item 3</a></li>
+                                   {menu}
                               </ul>
                          </div>
-                         <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                         <Link to={'/'} className="normal-case text-xl"><img className='w-32' src={logo} alt="" /></Link>
                     </div>
-                    <div className="hidden lg:flex border">
-                         <ul className="menu menu-horizontal px-1">
-                              <li><a>Item 1</a></li>
-                              <li tabIndex={0}>
-                                   <a>
-                                        Parent
-                                        <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                                   </a>
-                                   <ul className="p-2">
-                                        <li><a>Submenu 1</a></li>
-                                        <li><a>Submenu 2</a></li>
-                                   </ul>
-                              </li>
-                              <li><a>Item 3</a></li>
+                    <div className="hidden lg:flex lg:items-center ">
+                         <ul className="menu menu-horizontal px-2">
+                              {menu}
                          </ul>
+                         <div className="ml-8 flex items-center gap-6">
+                              <div className='relative'>
+                                   <label htmlFor="my-drawer-4" className="drawer-button cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                                   </svg>
+                                   <div className="badge badge-xs bg-[#EA2A00] border-none absolute left-[50%] bottom-[50%]">1</div>
+                                   </label>
+                              </div>
+                              <button className="bg-[#EA2A00] text-white px-5 py-2 rounded-2xl font-semibold">Confirm Order</button>
+                         </div>
                     </div>
-                    <div className="w-full justify-end border">
-                         <a className="btn">Get started</a>
-                    </div>
+
                </div>
 
-               {/* for cart opening */ }
-               <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary">cart</label>
+               {/* for cart opening */}
+               {/* <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary">cart</label> */}
           </div>
      );
 };
