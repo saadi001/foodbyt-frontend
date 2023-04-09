f4cca68b55ea0db2e1a9c20a629b3ee251bab82d
Sheikh sadi <sheikhsadi1620@gmail.com>
2023-04-09 14:00:00 +0600

    useToken hooks added

---
+++ src/Hooks/useToken.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+
+const useToken = email =>{
+     const [token, setToken] = useState('')
+
+     useEffect(()=>{
+          fetch(`https://foodbyt-backend.vercel.app/jwt?email=${email}`)
+          .then(res => res.json())
+          .then(data => {
+               console.log(data.accessToken)
+               if(data.accessToken){
+                    localStorage.setItem('accessToken', data.accessToken)
+                    setToken(data.accessToken)
+               }
+          })
+     },[email])
+
+     return [token];
+
+}
+
+export default useToken;
--- src/pages/Shared/Navbar/Navbar2.js
+++ src/pages/Shared/Navbar/Navbar2.js
@@ -4,12 +4,12 @@ import logo from "../../../Asset/logo/logo1.png"
 
 const Navbar2 = () => {
      return (
-          <div className='px-5 md:px-8 lg:px-20 z-50'>
+          <div className='px-3 md:px-4 lg:px-8 z-50'>
                <div className='py-2 flex justify-between items-center'>
                     <Link to={'/'}><img className='w-24 md:w-32' src={logo} alt="" /></Link>
                     <Link to={'/'} className='bg-primary px-3 py-2 text-white rounded-3xl hidden lg:inline-block'>Go to home page</Link>
                     
-                         <label htmlFor="my-drawer-2" className="cursor-pointer p-1 drawer-button lg:hidden"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                         <label htmlFor="my-drawer-2" className="cursor-pointer p-[6px] hover:bg-slate-900/10 rounded drawer-button lg:hidden"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
                          </svg>
                          </label>
