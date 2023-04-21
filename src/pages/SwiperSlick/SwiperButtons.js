a7654ddb1f7fcb57ec894c8eb7df5b6105830e3e
Sheikh sadi <sheikhsadi1620@gmail.com>
2023-04-22 01:51:04 +0600

    swiper slick updated in landing page

--- src/pages/SwiperSlick/SwiperButtons.js
+++ src/pages/SwiperSlick/SwiperButtons.js
@@ -4,9 +4,15 @@ import { useSwiper } from 'swiper/react';
 const SwiperButtons = () => {
      const swiper = useSwiper()
      return (
-          <div>
-               <button onClick={()=>swiper.slidePrev()}>prev</button>
-               <button onClick={()=>swiper.slideNext()}>next</button>
+          <div className='flex justify-end gap-2 mt-2'>
+               <button  className='bg-primary text-white rounded-full p-[6px] ' onClick={() => swiper.slidePrev()}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+               </svg>
+               </button>
+               <button className='bg-primary text-white rounded-full p-[6px]' onClick={() => swiper.slideNext()}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+               </svg>
+               </button>
           </div>
      );
 };
--- src/pages/SwiperSlick/SwiperSlick.js
+++ src/pages/SwiperSlick/SwiperSlick.js
@@ -7,56 +7,57 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
 import SwiperButtons from './SwiperButtons';
+import { useQuery } from '@tanstack/react-query';
+import { Link } from 'react-router-dom';
+import Loading2 from '../Loading/Loading2';
 
 const SwiperSlick = () => {
+     const {data: advertisement=[], isLoading} = useQuery({
+          queryKey: ["advertisement"],
+          queryFn: async ()=>{
+              const res = await fetch('https://foodbyt-backend.vercel.app/advertisement')
+              const data = await res.json()
+              return data;
+          }
+     })
+
+     if(isLoading){
+          return <Loading2></Loading2>
+     }
      return (
           <div className=''>
                <Swiper
-                    spaceBetween={20}
+                    spaceBetween={5}
                     slidesPerView={3}
-                    // autoplay={{
-                    //      delay: 2000,
-                    //      disableOnInteraction: false,
-                    //    }}
+                    autoplay={{
+                         delay: 2000,
+                         disableOnInteraction: false,
+                       }}
                     // pagination={{
                     //      clickable: true,
                     // }}
                     // navigation={true}
+                    breakpoints={{
+                         640: {
+                           spaceBetween: 10,
+                         },
+                         
+                       }}
                     modules={[Autoplay, Pagination, Navigation]}
                     className="mySwiper"
                     onSlideChange={() => console.log('slide change')}
                     onSwiper={(swiper) => console.log(swiper)}
                >
-                    <SwiperSlide>
-                         <div className='w-full relative'>
-                              <img className='w-full z-0' src={img} alt="" />
-                              <div className='absolute bg-white font-semibold z-50 left-2 right-2 bottom-2 p-2 text-sm capitalize'>hello</div>
-                         </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                         <div className='w-full relative'>
-                              <img className='w-full z-0' src={img} alt="" />
-                              <div className='absolute bg-white font-semibold z-50 left-2 right-2 bottom-2 p-2 text-sm capitalize'>hello</div>
-                         </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                         <div className='w-full relative'>
-                              <img className='w-full z-0' src={img} alt="" />
-                              <div className='absolute bg-white font-semibold z-50 left-2 right-2 bottom-2 p-2 text-sm capitalize'>hello</div>
-                         </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                         <div className='w-full relative'>
-                              <img className='w-full z-0' src={img} alt="" />
-                              <div className='absolute bg-white font-semibold z-50 left-2 right-2 bottom-2 p-2 text-sm capitalize'>hello</div>
-                         </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                         <div className='w-full relative'>
-                              <img className='w-full z-0' src={img} alt="" />
-                              <div className='absolute bg-white font-semibold z-50 left-2 right-2 bottom-2 p-2 text-sm capitalize'>hello</div>
-                         </div>
-                    </SwiperSlide>
+                    {
+                         advertisement?.map(ad => <SwiperSlide>
+                              <div className='w-full relative'>
+                                   <Link to={ad?.link}>
+                                   <div className='md:h-[160px] h-[120px]'><img className='w-full h-full object-cover z-0' src={ad?.image} alt="" /></div>
+                                   <div className='absolute bg-white font-semibold z-50 left-2 right-2 bottom-2 p-2 text-sm capitalize'>{ad?.name}</div></Link>
+                              </div>
+                         </SwiperSlide>)
+                    }
+                    
                     <SwiperButtons></SwiperButtons>
                     
                </Swiper>
