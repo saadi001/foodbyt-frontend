282c1be0373c9c0eca37a5508b448ead6aac6ec1
saadi001 <sheikhsadi1620@gmail.com>
2023-10-15 04:11:52 +0600

    changed the landing page photo

--- src/pages/Home/Landingpage/Landingpage.js
+++ src/pages/Home/Landingpage/Landingpage.js
@@ -1,8 +1,9 @@
 import { ChevronRightIcon, MapPinIcon } from '@heroicons/react/24/solid';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { } from 'react';
 import food from '../../../Asset/Image/Food1.png'
 import SwiperSlick from '../../SwiperSlick/SwiperSlick';
 import './Landingpage.css'
+import foodImage from "../../../Asset/Image/FoodImage.png"
 
 
 const Landingpage = () => {
@@ -33,7 +34,7 @@ const Landingpage = () => {
           <div className='background'>
                {/* <Navbar></Navbar> */}
                <div className='min-h-screen flex mx-5 md:mx-8 lg:mx-28 xl:mx-32 2xl:max-w-7xl 2xl:mx-auto'>
-                    <div className='md:w-1/2 w-full relative flex items-center'>
+                    <div className='lg:w-1/2 w-full relative flex items-center'>
                          <div className='w-full'>
                               <p className='text-lg mt-16 md:text-xl font-medium flex items-center gap-1'><MapPinIcon className='w-5 h-5 text-[#EA2A00]'></MapPinIcon> Daffodil International University</p>
                               <p className='text-[36px] md:text-[42px] font-bold my-3'>Order your favourite <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-[#EA2A00] relative inline-block">
@@ -43,11 +44,11 @@ const Landingpage = () => {
                               {/* slider  */}
                               <p className='mt-24 md:mt-16 capitalize font-semibold text-sm mb-3 text-primary flex items-center gap-2'>Top selling products <ChevronRightIcon className='w-[14px] h-[14px]'></ChevronRightIcon></p>
                               
-                              <div className='w-full'><SwiperSlick></SwiperSlick></div>
+                              <div className='w-full '><SwiperSlick></SwiperSlick></div>
                          </div>
                     </div>
-                    <div className='w-1/2 hidden md:flex items-center'>
-                         <img className=' w-full' src={food} alt="" />
+                    <div className='w-1/2 hidden  lg:flex items-center justify-center'>
+                         <img className=' w-9/12 mt-12 ' src={foodImage} alt="" />
                     </div>
                </div>
           </div>
